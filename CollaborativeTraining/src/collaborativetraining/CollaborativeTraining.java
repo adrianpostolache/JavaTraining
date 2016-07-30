42e5ba1f1d35d5f1229f592a507926be57af48cf
Adrian P <VoX@VoX-PC>
2016-07-30 13:45:53 +0300

    Demo in training

--- CollaborativeTraining/src/collaborativetraining/CollaborativeTraining.java
+++ CollaborativeTraining/src/collaborativetraining/CollaborativeTraining.java
@@ -14,8 +14,28 @@ public class CollaborativeTraining {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int sum = a + b;
+        int prod = a * b;
+        System.out.println(a + " + " + b + " = " + sum);
+        System.out.println(a + " * " + b + " = " + prod);
+        // strings
+        String d, e, f, g;
+        d = "My name is ";
+        e = "Paul";
+        f = d + e;
+        g = String.valueOf(a + b);
+        System.out.println(f);
+        System.out.println("Number converted to string is: " + g);
+        //Double
+        double h = Double.parseDouble("10.3324");
+        h = Math.toRadians(h);
+        System.out.println("Math.sin(" + h + ") =" + Math.sin(h));
+        //miracle
+        System.out.println("sin(a)*sin(a) + cos(a)*cos(a) = " + (Math.sin(h)* Math.sin(h) + Math.cos(h)* Math.cos(h)));
     }
     
 }
